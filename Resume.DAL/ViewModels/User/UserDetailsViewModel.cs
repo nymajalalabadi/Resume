2c1759c7cb664434517d5767226bc2004e05de64
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-06-10 00:21:21 +0330

    Added Create , Edit And Details User View Model

---
+++ Resume.DAL/ViewModels/User/UserDetailsViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resume.DAL.ViewModels.User
+{
+    internal class UserDetailsViewModel
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string Mobile { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public DateTime CreateDate { get; set; }
+    }
+}
