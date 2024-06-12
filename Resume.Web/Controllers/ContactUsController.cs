260e3ff95af0a50c5e3e52b3ee39cc71b5773fa9
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-06-12 14:28:05 +0330

    Completed Contact Us Controller

--- Resume.Web/Controllers/ContactUsController.cs
+++ Resume.Web/Controllers/ContactUsController.cs
@@ -33,6 +33,19 @@ namespace Resume.Web.Controllers
                 return View(create);
             }
 
+            var result = await _contactUsService.CreateContactUs(create);
+
+            switch (result)
+            {
+                case CreateContactUsResult.Success:
+                    TempData[SuccessMessage] = "پیام شما با موفقیت ثبت شد. نتیجه از طریق ایمیل به شما اطلاع رسانی خواهد شد.";
+                    return RedirectToAction("ContactUs");
+
+                case CreateContactUsResult.Error:
+                    TempData[ErrorMessage] = "خطایی رخ داده است لطفا مجدد تلاش کنید.";
+                    break;
+            }
+
             return View();
         }
 
