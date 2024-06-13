e0d1f9a6ba432e950ea1615e70d2a62db208fc03
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-06-13 18:43:00 +0330

    Added EnumExtensions

---
+++ Resume.Bussines/Extensions/EnumExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resume.Bussines.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetEnumName(this Enum myEnum)
+        {
+            var enumDisplayName = myEnum.GetType()
+                .GetMember(myEnum.ToString())
+                .FirstOrDefault();
+
+            if (enumDisplayName != null)
+            {
+                return enumDisplayName.GetCustomAttribute<DisplayAttribute>().GetName();
+            }
+
+            return "";
+        }
+    }
+
+}
