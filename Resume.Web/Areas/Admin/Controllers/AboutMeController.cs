9b778c28efc3d4496deec7a9ea329cbafb399686
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-06-15 14:35:34 +0330

    Added AboutMeController For Admin

---
+++ Resume.Web/Areas/Admin/Controllers/AboutMeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Resume.Bussines.Services.Interface;
+
+namespace Resume.Web.Areas.Admin.Controllers
+{
+    public class AboutMeController : Controller
+    {
+        #region Constructor
+
+        private readonly IAboutMeService _aboutMeService;
+
+        public AboutMeController(IAboutMeService aboutMeService)
+        {
+            _aboutMeService = aboutMeService;
+        }
+
+        #endregion
+
+
+        #region Actions
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #endregion
+    }
+}
