dcf73e14c05301c0a19c9421eceacde621d8521b
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-06-25 11:50:30 +0330

    Added Experience View Component

---
+++ Resume.Web/Components/ExperienceViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Resume.Bussines.Services.Interface;
+
+namespace Resume.Web.Components
+{
+    public class ExperienceViewComponent : ViewComponent
+    {
+        #region Constructor
+
+        private readonly IExperienceService _experienceService;
+
+        public ExperienceViewComponent(IExperienceService experienceService)
+        {
+            _experienceService = experienceService;
+        }
+
+        #endregion
+
+        #region Method
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var model = await _experienceService.GetAllExperienceViewModel();
+
+            return View("Experience", model);
+        }
+
+        #endregion
+    }
+}
