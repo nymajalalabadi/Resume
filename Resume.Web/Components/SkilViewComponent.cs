f4e5feb70be68ea84249588da0f95b93c57161a5
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-06-27 11:11:52 +0330

    Added Skil At Client Site

---
+++ Resume.Web/Components/SkilViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Resume.Bussines.Services.Implementation;
+using Resume.Bussines.Services.Interface;
+
+namespace Resume.Web.Components
+{
+    public class SkilViewComponent : ViewComponent
+    {
+        #region Constructor
+
+        private readonly ISkilService _skilService;
+
+        public SkilViewComponent(ISkilService skilService)
+        {
+            _skilService = skilService;
+        }
+
+        #endregion
+
+        #region Method
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var model = await _skilService.GetAllSkilViewModel();
+
+            return View("Skil", model);
+        }
+
+        #endregion
+    }
+}
